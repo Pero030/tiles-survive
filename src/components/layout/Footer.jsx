8299683c74fc122e15e86469d9d3b6b5897561c5
Pero030 <perobbm@gmail.com>
2026-07-26 20:37:35 +0200

    Add files via upload

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,19 +1,7 @@
-import { Link } from 'react-router-dom';
-import { useLanguage } from '../../context/LanguageContext.jsx';
-
 export function Footer() {
-  const { t } = useLanguage();
-
   return (
     <footer className="site-footer">
-      <p>{t('footerCopy')}</p>
-      <nav className="footer-links" aria-label="Footer navigation">
-        <Link to="/heroes">{t('navHeroes')}</Link>
-        <Link to="/events">{t('navEvents')}</Link>
-        <Link to="/buildings">{t('navBuildings')}</Link>
-        <Link to="/tips">{t('navTips')}</Link>
-        <Link to="/faq">{t('navFaq')}</Link>
-      </nav>
+      <p>Created by ADA Leader Virus, Server #867</p>
     </footer>
   );
 }
